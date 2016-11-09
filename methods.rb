7da5b53c888789e5f458de3d128d4f823eda053b
Yang Li-Yi <joseph.lyyang@gmail.com>
2016-11-09 11:39:55 +0800

    More elegant way to define methods.

--- methods.rb
+++ methods.rb
@@ -1,8 +1,42 @@
-# Example: One way to define instance methods
+# # Example: One way to define instance methods
+
+# class SoftwareEngineer
+#   def code
+#     puts "I can code."
+#   end
+# end
+
+# elon = SoftwareEngineer.new
+# elon.code # "I can code."
+
+# class WebDeveloper < SoftwareEngineer
+#   def can_write_ruby?
+#     puts "Sure I can write ruby!"
+#   end
+
+#   def can_write_javascript?
+#     puts "Sure I can write javascript!"
+#   end
+# end
+
+# ly = WebDeveloper.new
+# ly.code
+# ly.can_write_ruby?
+# ly.can_write_javascript?
+
+# Example: Use a more elegant way to define instance methods
 
 class SoftwareEngineer
   def code
     puts "I can code."
+  end
+
+  def self.can_write(*args)
+    args.each do |name|
+      define_method("can_write_#{name}?") do
+        puts "Sure I can write #{name}!"
+      end
+    end
   end
 end
 
@@ -10,13 +44,7 @@ elon = SoftwareEngineer.new
 elon.code # "I can code."
 
 class WebDeveloper < SoftwareEngineer
-  def can_write_ruby?
-    puts "Sure I can write ruby!"
-  end
-
-  def can_write_javascript?
-    puts "Sure I can write javascript!"
-  end
+  can_write :ruby, :javascript
 end
 
 ly = WebDeveloper.new
